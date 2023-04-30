0eb56bac4e77eb9e5aaa96e8c0a1536d2d0ec658
RebiscoSpecial <125041062+RebiscoSpecial@users.noreply.github.com>
2023-04-30 19:46:20 +0800

    Add files via upload

---
+++ hanna1script.js
@@ -0,0 +1,15 @@
+function toggleEnvelope() {
+  const innerPolygon = document.getElementById("inner-polygon");
+  const outer = document.getElementById("outer");
+  const closeBtn = document.getElementById("close-btn");
+  const message1 = document.getElementById("message-1");
+  const message2 = document.getElementById("message-2");
+  
+  const heartBtn = document.getElementById("heart-btn");
+  innerPolygon.classList.toggle("inner-open");
+  outer.classList.toggle("outer-open");
+  heartBtn.classList.toggle("hide");
+  closeBtn.classList.toggle("show");
+  message1.classList.toggle("hide");
+  message2.classList.toggle("show");
+}
